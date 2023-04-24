63f8176c44057bab377ef70868d6fff99bb4db8e
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2023-04-24 16:17:33 +0530

    provider first

--- lib/main.dart
+++ lib/main.dart
@@ -57,8 +57,26 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.location_on),
+                    SizedBox(width: 16.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('123 Main Street'),
+                          Text('New York, NY 10001'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               SizedBox(
-                height: 30,
+                height: 10,
               ),
               Container(
                 padding: EdgeInsets.all(16.0),
@@ -91,126 +109,257 @@ class HomeScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  SizedBox(width: 16.0),
                   Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.blueAccent,
-                    ),
-                    height: 300,
-                    width: 250,
-                    margin: EdgeInsets.only(right: 16.0),
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 30.0,
-                          child: Icon(Icons.money, size: 35.0, color: Colors.white),
-                          backgroundColor: Colors.green,
-                        ),
-                        SizedBox(height: 16.0),
-                        Text(
-                          'Rs.10,000',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          'Total Earning',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
                       color: Colors.greenAccent,
-                    ),
-                    height: 300,
-                    width: 250,
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 30.0,
-                          child: Icon(Icons.attach_money, size: 35.0, color: Colors.white),
-                          backgroundColor: Colors.blue,
-                        ),
-                        SizedBox(height: 16.0),
-                        Text(
-                          'Rs.25,000',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          'Gross Earning',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Container(
-                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Colors.blueAccent,
                     ),
-                    height: 300,
-                    width: 250,
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    height: 320,
+                    width: 380,
+                    child: Row(
+                     // mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        CircleAvatar(
-                          radius: 30.0,
-                          child: Icon(Icons.file_download_done_rounded, size: 35.0, color: Colors.white),
-                          backgroundColor: Colors.blue,
+                        Icon(
+                          Icons.arrow_left,
+                          size: 50,
                         ),
-                        SizedBox(height: 16.0),
                         Text(
-                          '40',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          'Order Received',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+                          'Swipe to show booked service',
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                          fontSize: 20),
                         ),
                       ],
                     ),
                   ),
+                  Container(
+                    width: 380,
+                    margin: EdgeInsets.only(left: 15,right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.only(top: 30, left: 16, right: 16, bottom: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                        //  mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // IconButton(
+                            //   icon: Icon(Icons.arrow_back),
+                            //   onPressed: () {},
+                            // ),
+                            Text(
+                              'Booked Service',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 40,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 32),
+                        Row(
+                          children: [
+                            Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images.jpeg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Ankit Dhattarwal',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  '7015216280',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Service Type',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                            ),
+                            Text(
+                              'Plumber',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Service Price',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                            ),
+                            Text(
+                              'Rs 599',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Service Date',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                            ),
+                            Text(
+                              'Mon, 32 Jun',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  // SizedBox(width: 16.0),
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.circular(10),
+                  //     color: Colors.greenAccent,
+                  //   ),
+                  //   height: 320,
+                  //   width: 300,
+                  //   padding: EdgeInsets.only(top: 30, left: 16, right: 16, bottom: 16),
+                  //   child: Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: [
+                  //       CircleAvatar(
+                  //         radius: 30.0,
+                  //         child: Icon(Icons.attach_money, size: 35.0, color: Colors.white),
+                  //         backgroundColor: Colors.blue,
+                  //       ),
+                  //       SizedBox(height: 16.0),
+                  //       Text(
+                  //         'Rs.25,000',
+                  //         style: TextStyle(
+                  //           color: Colors.white,
+                  //           fontSize: 24.0,
+                  //           fontWeight: FontWeight.bold,
+                  //         ),
+                  //       ),
+                  //       SizedBox(height: 8.0),
+                  //       Text(
+                  //         'Gross Earning',
+                  //         style: TextStyle(
+                  //           fontSize: 16.0,
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Colors.white,
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  // SizedBox(width: 16.0),
+                  // Container(
+                  //   margin: EdgeInsets.only(right: 16),
+                  //   decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.circular(10),
+                  //     color: Colors.blueAccent,
+                  //   ),
+                  //   height: 320,
+                  //   width: 300,
+                  //   padding: EdgeInsets.only(top: 30, left: 16, right: 16, bottom: 16),
+                  //   child: Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: [
+                  //       CircleAvatar(
+                  //         radius: 30.0,
+                  //         child: Icon(Icons.file_download_done_rounded, size: 35.0, color: Colors.white),
+                  //         backgroundColor: Colors.blue,
+                  //       ),
+                  //       SizedBox(height: 16.0),
+                  //       Text(
+                  //         '40',
+                  //         style: TextStyle(
+                  //           color: Colors.white,
+                  //           fontSize: 24.0,
+                  //           fontWeight: FontWeight.bold,
+                  //         ),
+                  //       ),
+                  //       SizedBox(height: 8.0),
+                  //       Text(
+                  //         'Order Received',
+                  //         style: TextStyle(
+                  //           fontSize: 16.0,
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Colors.white,
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                 ],
               ),
             ),
           ),
-
           ],
           ),
         ),
